42e2c63cadb2665b11c042bddf67acd9da82ba26
JamesonWelch <jamesonb.welch@gmail.com>
2020-06-04 16:05:20 -0700

    m

--- lists/migrations/0001_initial.py
+++ lists/migrations/0001_initial.py
@@ -18,5 +18,5 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
             ],
-        ),
+        ), 
     ]
